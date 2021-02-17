e361dda21718dfa56c23f87a6de6a8aecebf0225
Stevie Comyn <stevie@comyn.dev>
2021-02-17 15:02:23 +0000

    Added the logo to the settings.

--- settings-page.php
+++ settings-page.php
@@ -24,7 +24,10 @@ function pn_acc_settings_init()
 	{
 		// This sets up the settings form.
 		echo '<div class="wrap">
+		<div class="pn-acc-settings-header-wrapper">
+		<img src="'.trailingslashit( plugin_dir_path( __FILE__ ) ) .'assets/images/pn-delta.svg" width="28" height="28">
 		<h1>PageNorth - My Account Customisations</h1>
+		</div>
 		<form method="post" action="options.php">';
 
 			settings_fields( 'pn_acc_settings' ); // settings group name
