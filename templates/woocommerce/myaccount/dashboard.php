c8d823ec09abe70d1e0834d07b81bd8dc5e0e80c
Stevie Comyn <stevie@comyn.dev>
2021-04-01 11:12:07 +0100

    removed duplicate description (moved to above)

--- templates/woocommerce/myaccount/dashboard.php
+++ templates/woocommerce/myaccount/dashboard.php
@@ -266,24 +266,8 @@ include('../../../assets/config.php');
     
 ?>
 
-<p>
-	<?php
-	/* translators: 1: Orders URL 2: Address URL 3: Account URL. */
-	$dashboard_desc = __( 'From your account dashboard you can view your <a href="%1$s">order history</a>, manage your <a href="%2$s">billing address</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce' );
-	if ( wc_shipping_enabled() ) {
-		/* translators: 1: Orders URL 2: Addresses URL 3: Account URL. */
-		$dashboard_desc = __( 'From your account dashboard you can view your <a href="%1$s">order history</a>, manage your <a href="%3$s">shipping and billing addresses</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce' );
-	}
-	printf(
-		wp_kses( $dashboard_desc, $allowed_html ),
-		esc_url( wc_get_endpoint_url( 'orders' ) ),
-		esc_url( wc_get_endpoint_url( 'edit-address' ) ),
-		esc_url( wc_get_endpoint_url( 'edit-account' ) )
-	);
-	?>
-</p>
-
 </div><!-- Close "#pn_acc"  -->
+
 <?php
 	/**
 	 * My Account dashboard.
